9c133b9445d7c4bae7bf21243fa0ca7e0188a349
Joshua <mramos29@ucol.mx>
2020-12-07 10:48:02 -0600

    Starting

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
-export default class App {}
+export default class App {
+    sumarParesFor() {
+        for (let i=1; i<=20; i = i+1) {
+            console.log(i);
+        }
+    }
+}
 
 let app = new App();
+
+console.log(app.sumarParesFor);
