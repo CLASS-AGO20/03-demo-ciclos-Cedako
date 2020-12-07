fa389216d9cef60b43140c05d8117efbdc47cfce
Joshua <mramos29@ucol.mx>
2020-12-07 10:59:28 -0600

    sumarParesFor

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 export default class App {
     sumarParesFor() {
+        let suma = 0;
         for (let i=1; i<=20; i = i+1) {
-            console.log(i);
+            if(i%2 === 0){
+                suma = suma + i;
+            }
         }
+        return suma;
     }
 }
 
 let app = new App();
 
-console.log(app.sumarParesFor);
+console.log(app.sumarParesFor());
